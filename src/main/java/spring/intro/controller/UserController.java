cbfe50016851c8a1f96d30e53f3c296ce922f996
Borisko130 <krimenyukb@gmail.com>
2020-10-21 15:18:09 +0300

    Fixed codestyle

--- src/main/java/spring/intro/controller/UserController.java
+++ src/main/java/spring/intro/controller/UserController.java
@@ -1,9 +1,7 @@
 package spring.intro.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
